445b905bf7ac5b36488509d42650caf958d23619
Subrato Sarkar <subratosarkar29@gmail.com>
2020-10-26 20:19:05 -0400

    updated code

--- config/groups.js
+++ config/groups.js
@@ -4,8 +4,8 @@ module.exports = {
     configure: {
         smoke: {
             specs: [
-                'src/specs/smoke/login.js',
-                'src/specs/smoke/cities.js'
+                 'src/specs/smoke/login.js',
+                 'src/specs/smoke/cities.js'
                 // 'src/specs/smoke/help.js'
             ]
         },
